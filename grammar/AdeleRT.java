05388b5748dbad96232e76c568ba2466523aad57
ajchuang <alfred.jenchieh.huang@gmail.com>
2015-03-02 22:30:08 -0500

    update string templates

--- grammar/AdeleRT.java
+++ grammar/AdeleRT.java
@@ -1,4 +1,7 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.stringtemplate.v4.*;
+import org.stringtemplate.v4.misc.*;
 
 public class AdeleRT {
     public static void main (String[] args) throws Exception {
@@ -11,7 +14,14 @@ public class AdeleRT {
         CommonTokenStream tokens = new CommonTokenStream (lexer);
         adeleParser parser = new adeleParser (tokens);
         parser.addParseListener (new adeleListenerImpl ());
-        parser.prog ();
-        //System.out.println (parser.eval());                                              
+        ParseTree pt = parser.prog ();
+    
+        /* do the string template things */
+        STGroupFile stg = new STGroupFile ("./template/group.stg");
+        stg.registerModelAdaptor (ParserRuleContext.class, new STModelAdaptor ());
+        ST t = stg.getInstanceOf ("prog");
+        t.add ("assign", pt);
+        System.out.println (t.render ());
+    
     }
 }
--- grammar/adele_make.sh
+++ grammar/adele_make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.class
 
-java  -cp ../libs/antlr-4.5-complete.jar:./  org.antlr.v4.Tool ./adele.g4
-javac -cp ../libs/antlr-4.5-complete.jar:./  adele*.java
-javac -cp ../libs/antlr-4.5-complete.jar:./  Adele*.java
+java  -cp ../libs/antlr-4.5-complete.jar:../libs/ST-4.0.8.jar:./  org.antlr.v4.Tool ./adele.g4
+javac -cp ../libs/antlr-4.5-complete.jar:../libs/ST-4.0.8.jar:./  adele*.java
+javac -cp ../libs/antlr-4.5-complete.jar:../libs/ST-4.0.8.jar:./  Adele*.java
