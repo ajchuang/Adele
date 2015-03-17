2b1aa4ee670c80007c25aa71036241ce9c307324
Jie-Gang Kuang <jk3735@columbia.edu>
2015-03-17 19:43:31 -0400

    Remove include and move the files to src

--- build/adele_run.sh
+++ build/adele_run.sh
@@ -1,3 +1,3 @@
 #make
 #java -cp ../lib/antlr-4.5-complete.jar:. org.antlr.v4.runtime.misc.TestRig adele prog -gui
-java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:../include:. AdeleRT ../samples/test_000.adele
+java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:. AdeleRT ../samples/test_000.adele
