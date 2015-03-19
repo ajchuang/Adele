01c1b40ce8c5afd2979506c6655e47d43d0a3109
Jie-Gang Kuang <jk3735@columbia.edu>
2015-03-19 11:23:51 -0400

    Update simple test of StringTemplate

--- src/AdeleRT.java
+++ src/AdeleRT.java
@@ -5,23 +5,31 @@ import org.stringtemplate.v4.misc.*;
 
 public class AdeleRT {
     public static void main (String[] args) throws Exception {
-        
         if (args.length == 0)
             return;
-                
+
+        AdeleRT adele = new AdeleRT();
+
         ANTLRFileStream in = new ANTLRFileStream (args[0]);
         adeleLexer lexer = new adeleLexer (in);
         CommonTokenStream tokens = new CommonTokenStream (lexer);
         adeleParser parser = new adeleParser (tokens);
         parser.addParseListener (new adeleListenerImpl ());
         ParseTree pt = parser.prog ();
-    
+        System.out.println("test: " + pt.toString());
+
         /* do the string template things */
-        STGroupFile stg = new STGroupFile ("../src/template/group.stg");
-        stg.registerModelAdaptor (ParserRuleContext.class, new STModelAdaptor ());
-        ST t = stg.getInstanceOf ("assign");
-        t.add ("assign", pt);
-        System.out.println (t.render ());
-    
+        adele.walkParseTree(pt);
+    }
+
+    public void walkParseTree(ParseTree pt) {
+        STGroupFile stg = new STGroupFile("../src/template/group.stg");
+        ST decl = stg.getInstanceOf("vardecl");
+        decl.add("vname", "a");
+        decl.add("value", "0");
+        ST func = stg.getInstanceOf("funcdef");
+        func.add("fname", "main");
+        func.add("body", decl.render());
+        System.out.println(func.render());
     }
 }
