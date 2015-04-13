a4d146f7bf8e0dcebe1ca1786f7c5f07566a4cf4
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-13 00:46:26 -0400

    Add optional file path for compiling Adele source codess

--- build/adele_run.sh
+++ build/adele_run.sh
@@ -1,5 +1,10 @@
+#set -x
+#make clean
 #make
 #java -cp ../lib/antlr-4.5-complete.jar:. org.antlr.v4.runtime.misc.TestRig adele prog -gui
-#java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:. AdeleRT ../samples/test_simple.adele
-java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:. AdeleRT ../samples/codes/sample.adele
+src=../samples/codes/sample.adele
+if [ -f "$1" ]; then
+	src=$1
+fi
+java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:. AdeleRT $src
 open output.html
