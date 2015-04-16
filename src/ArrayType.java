0b22ae7ae3fa5717c939f44509362ca7d1f7359b
Zixuan Gong <zg2203@columbia.edu>
2015-04-16 10:31:39 -0400

    add ArrayType.java

---
+++ src/ArrayType.java
@@ -0,0 +1,13 @@
+public class ArrayType extends Symbol implements Type {
+  Type elementType;
+  int dimension;
+
+  public ArrayType(Type elementType, int dimension) {
+      super(elementType.getName() + " array");
+      this.elementType = elementType;
+      this.dimension = dimension;
+  }
+
+  public int getDimension() { return dimension; }
+  public int getTypeIndex() { return 0; }
+}
