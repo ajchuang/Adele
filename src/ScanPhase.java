501f6e9f9d8308e4c9b771db3639fb6bc28bc863
Zixuan Gong <zg2203@columbia.edu>
2015-04-16 02:56:41 -0400

    again fix problems in ScanPhase. tested

--- src/ScanPhase.java
+++ src/ScanPhase.java
@@ -11,6 +11,7 @@ class ScanPhase extends adeleBaseListener {
     GlobalScope globals;
     Scope currentScope;
     ParseTreeProperty<Type> types = new ParseTreeProperty<Type>();
+    boolean check_type = false;
 
     public ScanPhase (SymbolTable symtab) {
         globals = symtab.globals;
@@ -32,27 +33,29 @@ class ScanPhase extends adeleBaseListener {
         GroupSymbol gs = new GroupSymbol("group " + typeName, globals);
         globals.define(gs);
         currentScope = gs;
+        check_type = true;
+    }
+
+    public void exitType_dec_item(adeleParser.Type_dec_itemContext ctx) {
+        String f_name = ctx.ID().getText();
+        /* if type hasn't been defined, error is handled by exitType() */
+        Type f_type = types.get(ctx.type());
+
+        VariableSymbol vs = new VariableSymbol(f_name, f_type);
+        if (!currentScope.define (vs)) {
+            err(ctx.start.getLine(),
+                "Field named '"+f_name+"' already exists");
+        }
     }
 
     /* members' types should already be defined */
     public void exitType_declaration (adeleParser.Type_declarationContext ctx) {
 
         /* save symbol name as 'group x', s.t. x can still be used as var name */
-        List<adeleParser.Type_dec_itemContext> fields = ctx.type_dec_item();
-        for (adeleParser.Type_dec_itemContext field: fields) {
-            String f_name = field.ID().getText();
-            /* if type hasn't been defined, error is handled by exitType() */
-            Type f_type = types.get(field.type());
-
-            VariableSymbol vs = new VariableSymbol(f_name, f_type);
-            if (!currentScope.define (vs)) {
-                err(ctx.start.getLine(),
-                    "Field named '"+f_name+"' already exists");
-            }
-        }
 
         print("  "+currentScope.toString());
         currentScope = globals;
+        check_type = false;
         // try {
         //     TypeGroup ut = new TypeGroup (typeName);
         //     err ("User-defined Type: " + ut);
@@ -61,57 +64,41 @@ class ScanPhase extends adeleBaseListener {
         // }
     }
 
+    /*
+     * return type and params' type should already be defined;
+     * there will only be global functions
+     */
     public void enterFunc (adeleParser.FuncContext ctx) {
         String name = ctx.ID().getText();
         Type type = (Type)globals.resolve(ctx.type().getText());
         FunctionSymbol fs = new FunctionSymbol("function " + name, type, globals);
         globals.define(fs);
         currentScope = fs;
+        check_type = true;
     }
 
-    /*
-     * return type and params' type should already be defined;
-     * there will only be global functions
-     */
+    public void exitPitem(adeleParser.PitemContext ctx) {
+        String p_name = ctx.ID().getText();
+        Type p_type = types.get(ctx.type());
+
+        VariableSymbol vs = new VariableSymbol(p_name, p_type);
+        if (!currentScope.define (vs))
+            err(ctx.start.getLine(),
+                "Param named '"+p_name+"' already exists");
+    }
+
+    public void exitPlist(adeleParser.PlistContext ctx) {
+        check_type = false; /* don't check type in the func block */
+    }
+
     public void exitFunc (adeleParser.FuncContext ctx) {
-        /*
-         * ZX: The following part is moved to BaseScope.java, in define()
-         *
-         * semantic check: no same name functions are allowed
-
-            Symbol s = globals.resolve (name);
-
-            TODO: we should use the errorhandler to handle this
-            if (s != null && s instanceof FunctionSymbol) {
-                err ("Function " + name + " is duplicated.");
-                System.exit (0);
-            }
-         */
-
-        adeleParser.PlistContext plist = ctx.plist ();
-        List<adeleParser.PitemContext> items = plist.pitem ();
-
-        for (int i=0; i<items.size(); ++i) {
-
-            adeleParser.PitemContext pitem = items.get (i);
-            String p_name = pitem.ID().getText();
-            Type p_type = types.get(pitem.type());
-            // if (ptype == null)
-            //     err("null");
-
-            VariableSymbol vs = new VariableSymbol(p_name, p_type);
-            if (!currentScope.define (vs))
-                err(ctx.start.getLine(),
-                    "Param named '"+p_name+"' already exists");
-        }
-
         print("  "+currentScope.toString());
         currentScope = globals;
     }
 
     public void exitType(adeleParser.TypeContext ctx) {
-        /* For now don't check types in group and func blocks */
-        if (currentScope != globals)
+        /* Don't check types func blocks */
+        if (!check_type)
             return;
 
         String typeStr = ctx.start.getText();
