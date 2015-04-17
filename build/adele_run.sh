9ef0901f1af0032b27870724bd311df5f0b8a9b6
linyuan9219 <ly2324@dyn-160-39-154-196.dyn.columbia.edu>
2015-04-17 17:51:21 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 37e8fa197483ed320d200da408a77ab0eed49c12, reversing
    changes made to 44a551e80f18584c6628d74a54ea000b7d254386.

---
+++ build/adele_run.sh
@@ -0,0 +1,20 @@
+#set -x
+#make clean
+#make
+#java -cp ../lib/antlr-4.5-complete.jar:. org.antlr.v4.runtime.misc.TestRig adele prog -gui
+src=../samples/codes/qsort.adele
+dst=./output.html
+
+if [ -f "$1" ]; then
+	src=$1
+
+    if [ -f "$2" ]; then
+        dst=$2
+    fi
+fi
+
+java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:. AdeleRT $src
+
+if [ -f $dst ]; then
+    open $dst
+fi
