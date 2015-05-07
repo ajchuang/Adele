41c30c997ed375737979e137cb6f05cd7e27ee2d
ajchuang <alfred.jenchieh.huang@gmail.com>
2015-05-07 15:29:43 -0400

    alter runtime name

--- test/singletest.sh
+++ test/singletest.sh
@@ -2,7 +2,7 @@ res=$(../test_static/debug.sh $1.adele)
 echo $res
 
 if [[ -z $(grep fail <<< $res) ]]; then
-  java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:../build AdeleRT ./$1.adele $1 1> trans_phase_output.txt 2> static_check_output.txt
+  java -cp ../lib/antlr-4.5-complete.jar:../lib/ST-4.0.8.jar:../build AdeleRunTime ./$1.adele $1 1> trans_phase_output.txt 2> static_check_output.txt
   node_modules/.bin/nodeunit rt_$1.js
 fi
 
